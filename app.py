8bb681252a0178d9b52c75824831a3faa4c621e5
garysgray <garys.gray@gmail.com>
2022-09-27 20:40:29 -0700

    clean up, more items and rooms

--- app.py
+++ app.py
@@ -63,53 +63,47 @@ def index():
 @app.route('/game/<int:id>', methods=['GET','POST'])
 def game(id):
 
-    #time.sleep(.3)
+    time.sleep(.2)
     try:
         db_player = DB_Player.query.get_or_404(id)
     except:
         return 'issues getting player thru id'
-    cmd = ""
-    my_ctrl.load_stuff(db_player)
-    # match my_ctrl.State:
-    #     case State.LOAD:   
-    #         my_ctrl.load_stuff(db_player)
-    #         cmd = "help"
-    #         my_ctrl.State = State.PLAY 
-    #     case State.PLAY:
-    #         my_ctrl.load_stuff(db_player)
-    #         cmd = db_player.cmd_info
+
+    match my_ctrl.State:
+        case State.LOAD:   
+            my_ctrl.load_stuff(db_player)
+            cmd = "help"
+            my_ctrl.State = State.PLAY 
+        case State.PLAY:
+            my_ctrl.load_stuff(db_player)
+            cmd = db_player.cmd_info
     
-    # #let loading catch up
-    # time.sleep(.3)
+    #let loading catch up
+    time.sleep(.3)
 
-    # cmd_info = my_ctrl.parse_it(cmd)
-    # my_ctrl.run_the_cmd(cmd_info)
+    cmd_info = my_ctrl.parse_it(cmd)
+    my_ctrl.run_the_cmd(cmd_info)
 
-    # #let run_cmd catch up
-    # time.sleep(.3)
+    #let run_cmd catch up
+    time.sleep(.3)
 
-    # #SAVING STUFF TO DB
-    # player_loc_dumped, player_inventory_dumped, rooms_inventories_dumped = my_ctrl.save_stuff()
-    # db_player.location = player_loc_dumped
-    # db_player.player_inventory = player_inventory_dumped
-    # db_player.room_inventory = rooms_inventories_dumped
+    #SAVING STUFF TO DB
+    player_loc_dumped, player_inventory_dumped, rooms_inventories_dumped = my_ctrl.save_stuff()
+    db_player.location = player_loc_dumped
+    db_player.player_inventory = player_inventory_dumped
+    db_player.room_inventory = rooms_inventories_dumped
 
-    # try:
-    #     db.session.commit()  
-    # except:
-    #     return 'issues saving after running cmd'
+    time.sleep(.2)
+
+    try:
+        db.session.commit()  
+    except:
+        return 'issues saving after running cmd'
 
     if request.method == 'POST':
 
         cmd = request.form['cmd']
-        #db_player.cmd_info = cmd.lower()
-        cmd_info = my_ctrl.parse_it(cmd.lower())
-        my_ctrl.run_the_cmd(cmd_info)
-
-        player_loc_dumped, player_inventory_dumped, rooms_inventories_dumped = my_ctrl.save_stuff()
-        db_player.location = player_loc_dumped
-        db_player.player_inventory = player_inventory_dumped
-        db_player.room_inventory = rooms_inventories_dumped
+        db_player.cmd_info = cmd.lower()
    
         try:
             db.session.commit()
